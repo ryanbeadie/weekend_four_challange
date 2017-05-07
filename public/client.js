b9acc0017aa5f8b34ace896ad671e2a6766e6b20
Ryan Beadie <ryansteelebeadie@yahoo.com>
2017-05-07 14:39:36 -0500

    still working on css

--- public/client.js
+++ public/client.js
@@ -16,12 +16,12 @@ function getListings(){
       console.log('response:', response);
 
       var img2 = '<img src="http://www.iconsplace.com/icons/preview/orange/apartment-256.png"style="width:60%" />';
-      var img = '<img src="http://files.erealtymedia.com/images/photo_not_available_240x180.jpg?&width=180&siteId=282&status=Active"style="width:60%" />';
+      var img = '<img src="https://t3.ftcdn.net/jpg/01/00/62/72/240_F_100627217_hh03qHpa6sIOcBXvmmMySYEDGEQZrP4O.jpg"style="width:60%" />';
       for (var i = 0; i < response.length; i++) {
         if (response[i].cost) {
-          $('#saleListings').append( '<div class="col-sm-2">' + img + '<p></p><br>List Price:$' + response[i].cost + '</p><p>Square Feet:' + response[i].sqft + 'sq.ft.</p><p>City:' + response[i].city + '</p></div>');
+          $('#saleListings').append( '<div class="col-sm-2">' + img + '<p>List Price: $' + response[i].cost + '<br>Square Feet: ' + response[i].sqft + 'sq.ft.<br>City: ' + response[i].city + '<br><br><br><p></p></div>');
         } else{
-          $('#rentListings').append( '<div class="col-sm-2">' + img2 + '<p>Monthly Rent:$'+ Math.floor (response[i].rent / 12)  +'</p><p>Square Feet:'+response[i].sqft+'sq.ft.</p><p>City:'+response[i].city+'</p></div>');
+          $('#rentListings').append( '<div class="col-sm-2">' + img2 + '<p>Monthly Rent: $'+ Math.floor (response[i].rent / 12)  +'<br>Square Feet: ' + response[i].sqft + 'sq.ft.<br>City: ' + response[i].city + '<br><br><br><p></p></div>');
         }  // end else
       }  // end for loop
     }  // end success
